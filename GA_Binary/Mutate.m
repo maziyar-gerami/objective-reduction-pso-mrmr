712121913e35cd72c4d6c1e060b21b50f415788b
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-30 00:00:00 +0330

    Mutate.m modified

---
+++ GA_Binary/Mutate.m
@@ -0,0 +1,16 @@
+function y=Mutate(x,mu)
+
+    
+    
+    nVar=numel(x);
+    
+    nmu=ceil(mu*nVar);
+    
+    j=randsample(nVar,nmu);
+    
+    y=x;
+    y(j)=1-x(j);    
+        
+
+   
+end
