c4f59a0c6052356638fd6404532ad5bf21536238
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-30 00:00:00 +0330

    fitness.m modified

---
+++ PSO/fitness.m
@@ -0,0 +1,16 @@
+function Z=fitness(x)
+
+
+Z=sum(x.^2); % sphere
+
+
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                   %
+%                          www.matlabnet.ir                         %
+%                   Free Download  matlab code and movie            %
+%                          Shahab Poursafary                        %
+%                                                                   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
